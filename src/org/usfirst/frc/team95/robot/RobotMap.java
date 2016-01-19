90cb3efff30fc14060444a707f7c5c2f6626378b
Daroc Alden <setupminimal@gmail.com>
2016-01-19 18:10:34 -0500

    Changed RobotMap to use SyncGroups.

--- src/org/usfirst/frc/team95/robot/RobotMap.java
+++ src/org/usfirst/frc/team95/robot/RobotMap.java
@@ -3,24 +3,32 @@ package org.usfirst.frc.team95.robot;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class RobotMap {
-	static  CANTalon backLeftMotor, backRightMotor, frontLeftMotor, frontRightMotor;
-	static RobotDrive rearDrive, frontDrive;
+	static CANTalon left1, left2, left3, right1, right2, right3;
 	static Joystick driveStick;
+	static RobotDrive robotDrive;
+	static SyncGroup left, right;
 	
 	public static void init() {
 		// Not actually mapped to the real locations on the robot
-    	backLeftMotor   = new CANTalon(0);
-    	backRightMotor  = new CANTalon(1);
-    	frontLeftMotor  = new CANTalon(2);
-    	frontRightMotor = new CANTalon(3);
+    	left1  = new CANTalon(0);
+    	left2  = new CANTalon(1);
+    	left3  = new CANTalon(2);
+    	right1 = new CANTalon(3);
+    	right2 = new CANTalon(4);
+    	right3 = new CANTalon(5);
     	
-    	// Ideally we'd use some smarter class (likely a SyncGroup or something like that),
-    	// but to make this simple I'm gonna just instantiate a drive controller for
-    	// the front and for the back
-    	rearDrive = new RobotDrive(backLeftMotor, backRightMotor);
-    	frontDrive = new RobotDrive(backLeftMotor, backRightMotor);
+    	// SyncGroups for each side.
+    	SpeedController[] leftTable = {left1, left2, left3};
+    	SpeedController[] rightTable = {right1, right2, right3};
+    	left = new SyncGroup(leftTable);
+    	left = new SyncGroup(rightTable);
+    	
+    	
+    	// The main drive train for the robot
+    	robotDrive = new RobotDrive(left, right);
     	
     	driveStick = new Joystick(0);
 	}
