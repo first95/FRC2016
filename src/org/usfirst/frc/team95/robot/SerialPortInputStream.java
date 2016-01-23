e34b0eaf3570de9d353b62c4e54870b52075b26a
John Walthour <john.walthour@gmail.com>
2016-01-23 12:29:25 -0500

    Added test MAVLink library.  Licensed under GPL3.

---
+++ src/org/usfirst/frc/team95/robot/SerialPortInputStream.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team95.robot;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import edu.wpi.first.wpilibj.SerialPort;
+
+public class SerialPortInputStream extends InputStream {
+	SerialPort sp;
+	
+	public SerialPortInputStream(SerialPort sp) {
+		this.sp = sp;
+	}
+	
+	@Override
+	public int read() throws IOException {
+		return sp.read(1)[0]; // reads 1 byte from the serial port
+	}
+
+}
