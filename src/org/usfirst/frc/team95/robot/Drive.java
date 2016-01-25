e019681b415b6559e327c46b8652b2426e39661f
Spencer <torr436@gmail.com>
2016-01-25 17:43:09 -0500

    actually added drive class this time

---
+++ src/org/usfirst/frc/team95/robot/Drive.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team95.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class Drive {
+	SpeedController left, right;
+	
+	//drives stuff
+	public Drive(SpeedController left, SpeedController right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	//pure tank controlls
+	public void tankDrive(double leftsp, double rightsp) {
+		left.set(leftsp * Math.abs(leftsp));
+		right.set(rightsp * Math.abs(rightsp));
+	}
+	
+	//squared arcade style drive
+	public void arcadeDrive(double y, double x) {
+		tankDrive((x+y)/2, (x-y)/2);
+	}
+	
+	//gets the joystick values for stuff
+	public void arcadeDrive(Joystick stick) {
+		arcadeDrive(stick.getY()*-1,stick.getX()*-1);
+	}
+}
