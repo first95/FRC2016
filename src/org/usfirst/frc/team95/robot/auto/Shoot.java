4deaae2b93bf991b6cae4dbe225be4a198a11c9b
Spencer <torr436@gmail.com>
2016-02-11 18:56:28 -0500

    forgot a file

---
+++ src/org/usfirst/frc/team95/robot/auto/Shoot.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.RobotMap;
+
+public class Shoot extends Auto{
+
+	@Override
+	public void init() {
+	//	RobotMap.shoot.setSetpoint(setpoint);
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
