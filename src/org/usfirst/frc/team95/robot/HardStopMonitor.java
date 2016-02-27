0107cffa5cba1c17b5933c7795fe02b31dd2cdc3
Daroc Alden <setupminimal@gmail.com>
2016-02-27 17:53:06 -0500

    Fix

--- src/org/usfirst/frc/team95/robot/HardStopMonitor.java
+++ src/org/usfirst/frc/team95/robot/HardStopMonitor.java
@@ -1,5 +1,21 @@
 package org.usfirst.frc.team95.robot;
 
-public class HardStopMonitor {
+import edu.wpi.first.wpilibj.Timer;
+
+public class HardStopMonitor implements PollableSubsystem {
+	Timer time = new Timer();
+
+	@Override
+	public void init() {
+		time.reset();
+		time.stop();
+	}
+
+	@Override
+	public void update() {
+		//if (RobotMap.pdb.getCurrent(15) > Constants.hardStopThreshold) {
+			//time.st
+		//}
+	}
 
 }
