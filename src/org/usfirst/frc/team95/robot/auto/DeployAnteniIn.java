6c3175e00cf03c4afa89125c1a023ee423dcc3a0
RoboticsNA <nicholas.arnold@mvrsd.org>
2016-04-13 23:10:49 -0400

    These two classes I forgot to add to the last commit. They are the auto moves.

---
+++ src/org/usfirst/frc/team95/robot/auto/DeployAnteniIn.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Timer;
+import org.usfirst.frc.team95.robot.auto.DeployAnteniOut;
+
+public class DeployAnteniIn extends Auto
+{
+
+	Timer timer = new Timer();
+	boolean done = false;
+	int out = DeployAnteniOut.out;
+	
+	@Override
+	public void init()
+	{
+		timer.reset();
+		timer.start();
+		RobotMap.shoot1L.set(-1);
+		RobotMap.shoot1R.set(1);
+	}
+
+	@Override
+	public void update()
+	{
+		if (timer.get() >= 1) {
+			done = true;
+			RobotMap.shoot1L.setSetpoint(0);
+			RobotMap.shoot1R.setSetpoint(0);
+			this.out = 0;
+		} else {
+			RobotMap.shoot1L.setSetpoint(-1);
+			RobotMap.shoot1R.setSetpoint(1);
+		}
+	}
+
+	@Override
+	public void stop()
+	{
+		this.out = 0;
+		RobotMap.shoot1L.setSetpoint(0);
+		RobotMap.shoot1R.setSetpoint(0);
+	}
+
+	@Override
+	public boolean done()
+	{
+		return done;
+	}
+
+}
---
+++ src/org/usfirst/frc/team95/robot/auto/DeployAnteniOut.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class DeployAnteniOut extends Auto{
+	
+	Timer timer = new Timer();
+	boolean done = false;
+	// Used to set in Anteni and out
+	public static int out = 0;
+
+	@Override
+	public void init()
+	{
+		timer.reset();
+		timer.start();
+		RobotMap.shoot1L.set(1);
+		RobotMap.shoot1R.set(-1);
+	}
+
+	@Override
+	public void update()
+	{
+		if (timer.get() >= 1) {
+			done = true;
+			RobotMap.shoot1L.setSetpoint(0);
+			RobotMap.shoot1R.setSetpoint(0);
+			this.out = 1;
+		} else {
+			RobotMap.shoot1L.setSetpoint(1);
+			RobotMap.shoot1R.setSetpoint(-1);
+		}
+		
+	}
+
+	@Override
+	public void stop()
+	{
+		this.out = 1;
+		RobotMap.shoot1L.setSetpoint(0);
+		RobotMap.shoot1R.setSetpoint(0);
+		
+	}
+
+	@Override
+	public boolean done()
+	{
+		return done;
+	}
+
+
+}
